d18c00438389a3083a9f9eecbb27c24898a9ec03
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2022-12-04 21:16:06 +0200

    Add auto suggest peer in contacts

--- peer_reminder_flutter/lib/peers/ModelContact.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Contact {
-  Contact(this.name, this.phone, this.email);
-  final String name, phone, email;
-
-  @override
-  String toString() => name;
-}
