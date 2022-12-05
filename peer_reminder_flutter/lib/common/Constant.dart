d23fee3f74344c94117d1cfdc19cbc6020ef0113
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2022-12-05 21:29:37 +0200

    Add validator forms for Task name, date and time, peer contact

--- peer_reminder_flutter/lib/common/Constant.dart
+++ peer_reminder_flutter/lib/common/Constant.dart
@@ -3,3 +3,6 @@ import 'package:flutter/material.dart';
 const double FONTSIZE_L = 15;
 const double FONTSIZE_XL = 18;
 const double FONTSIZE_XXL = 25;
+
+const String DATE_FORMAT = "dd-MM-yyyy";
+const String DATETIME_FORMAT = "dd-MM-yyyy hh:mm";
