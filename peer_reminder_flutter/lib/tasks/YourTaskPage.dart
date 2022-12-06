118337a6c1694296077e3f5cdf1a4384af0d9631
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2022-12-06 22:27:27 +0200

    Add confirm dialog when swipe to dismiss task

--- peer_reminder_flutter/lib/tasks/YourTaskPage.dart
+++ peer_reminder_flutter/lib/tasks/YourTaskPage.dart
@@ -2,11 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:peer_reminder_flutter/tasks/NewTaskPage.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 // Local imports
 import 'package:peer_reminder_flutter/common/Constant.dart' as constant;
 import 'package:peer_reminder_flutter/common/Util.dart';
-
 
 class YourTaskPage extends StatefulWidget {
   const YourTaskPage({super.key});
@@ -34,10 +34,9 @@ class _YourTaskPageState extends State<YourTaskPage> {
           itemBuilder: (context, i) {
             // Add divider if position is odd
             if (i.isOdd) return const Divider();
-
             final index = i ~/ 2;
 
-            return _onSwipeTask(index);
+            return _onSlideTask(index);
           }),
       floatingActionButton: FloatingActionButton(
         onPressed: _addNewTask,
@@ -58,7 +57,7 @@ class _YourTaskPageState extends State<YourTaskPage> {
     );
   }
 
-  Dismissible _onSwipeTask(int index){
+  Dismissible _onSwipeTask(int index) {
     final item = _taskList[index];
     return Dismissible(
       // Each Dismissible must contain a Key. Keys allow Flutter to
@@ -85,6 +84,149 @@ class _YourTaskPageState extends State<YourTaskPage> {
       ),
     );
   }
+
+  Slidable _onSlideTask(int itemIndex) {
+    return Slidable(
+      // Specify a key if the Slidable is dismissible.
+      key: UniqueKey(),
+
+      startActionPane: ActionPane(
+        motion: const DrawerMotion(),
+
+        // A pane can dismiss the Slidable.
+        dismissible: DismissiblePane(confirmDismiss: () {
+          _onConfirmDeleteTask();
+          return Future(() => true);
+        }, onDismissed: () {
+          _deleteTask(itemIndex);
+        }),
+
+        // All actions are defined in the children parameter.
+        children: [
+          SlidableAction(
+            // TODO: figure out to use stateful function here
+            onPressed: doNothing,
+            backgroundColor: Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+        ],
+      ),
+
+      endActionPane: ActionPane(
+        motion: const DrawerMotion(),
+        children: [
+          SlidableAction(
+            onPressed: doNothing,
+            backgroundColor: Color(0xFF7BC043),
+            foregroundColor: Colors.white,
+            icon: Icons.notifications_active_outlined,
+            label: 'Notify Peer',
+          ),
+        ],
+      ),
+
+      // The child of the Slidable is what the user sees when the
+      // component is not dragged.
+      child: ListTile(
+        title: Text(
+          _taskList[itemIndex],
+          style: _biggerFont,
+        ),
+      ),
+    );
+  }
+
+  void doNothing(BuildContext context) {
+    print("abc");
+  }
+
+  void _onConfirmDeleteTask() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        double width = MediaQuery.of(context).size.width;
+        double height = MediaQuery.of(context).size.height;
+        return AlertDialog(
+            backgroundColor: Colors.transparent,
+            contentPadding: EdgeInsets.zero,
+            elevation: 0.0,
+            content: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  width: width,
+                  padding: const EdgeInsets.all(15.0),
+                  decoration: const BoxDecoration(
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  child: Column(
+                    children: [
+                      // Text information
+                      const Text("This task will be deleted"),
+                      const Divider(),
+                      // Delete button
+                      _createConfirmDeleteButton(width, height),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                // Cancel button
+                _createCancelDeleteButton(width, height),
+              ],
+            ));
+      },
+    );
+  }
+
+  void _deleteTask(int itemIndex) {
+    setState(() {
+      _taskList.removeAt(itemIndex);
+    });
+  }
+
+  InkWell _createCancelDeleteButton(double width, double height) {
+    return InkWell(
+      child: Container(
+        width: width,
+        padding: const EdgeInsets.all(15.0),
+        decoration: const BoxDecoration(
+            color: Colors.white70,
+            borderRadius: BorderRadius.all(Radius.circular(10.0))),
+        child: const Center(
+          child: Text(
+            'Cancel',
+            style: TextStyle(fontSize: constant.FONTSIZE_XL, color: Colors.lightBlue),
+          ),
+        ),
+      ),
+      onTap: () => Navigator.of(context).pop(),
+    );
+  }
+
+  InkWell _createConfirmDeleteButton(double width, double height) {
+    return InkWell(
+      child: Container(
+        width: width,
+        // height: height*0.3,
+        padding: const EdgeInsets.all(5.0),
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10.0))),
+        child: const Center(
+          child: Text(
+            'Delete',
+            style: TextStyle(fontSize: constant.FONTSIZE_XL, color: Colors.red),
+          ),
+        ),
+      ),
+      // FIXME: add callback for delete here
+      onTap: () => Navigator.of(context).pop(),
+    );
+  }
+
   // -------------------------------------------------------------------
   // Private Utils
   Future<bool> _requestPermission() async {
