460e8d8081a165a8a35c0f4dccb498af97447dcc
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2022-12-15 00:03:07 +0200

    Better Uri query param encoder

--- peer_reminder_flutter/lib/common/Util.dart
+++ peer_reminder_flutter/lib/common/Util.dart
@@ -94,4 +94,11 @@ class Util {
     );
     return task;
   }
+
+  static String? encodeQueryParameters(Map<String, String> params) {
+    return params.entries
+        .map((MapEntry<String, String> e) =>
+            '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+        .join('&');
+  }
 }
