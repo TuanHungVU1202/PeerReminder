269f393c7ddb8a88a152ffd4b02ca3771fce1b15
Tuan-Hung VU <41147045+TuanHungVU1202@users.noreply.github.com>
2022-12-16 23:02:24 +0200

    Refactor YourTasks with AbstractTaskList (#1)
    * Create basic abstract class for both YourTask and MyTask
    
    * Refactor YourTasks using AbstractTaskList

---
+++ peer_reminder_flutter/lib/tasks/TaskTile.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'ViewTaskPage.dart';
+import 'model/Task.dart';
+import 'package:peer_reminder_flutter/common/UIConstant.dart';
+
+class TaskTile extends StatelessWidget {
+  final int taskIndex;
+  final List<String> _filteredTaskList;
+  final Task task;
+
+  const TaskTile(this._filteredTaskList, this.taskIndex,
+      {super.key, required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.zero,
+        margin: const EdgeInsets.only(left: 10),
+        child: Material(
+          // Create Material widget for each ListTile
+          child: ListTile(
+            // TODO: pass JSON string here instead just String title
+            onTap: () => _viewTask(context, _filteredTaskList[taskIndex]),
+            // selectedTileColor: Colors.lightBlue,
+            title: Text(
+              _filteredTaskList[taskIndex],
+              style: UIConstant.biggerFont,
+            ),
+            trailing: const Icon(Icons.home),
+          ),
+        ));
+  }
+
+  // -------------------------------------------------------------------
+  // Callbacks
+  void _viewTask(BuildContext context, String taskTitle) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        // Not preview => isPreview = false
+        builder: (BuildContext context) => ViewTaskPage(task, false),
+      ),
+    );
+  }
+}
