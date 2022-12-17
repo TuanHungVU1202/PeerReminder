ec3f46bc97cf4e8a3f0f802892341f99be81d428
Tuan-Hung VU <41147045+TuanHungVU1202@users.noreply.github.com>
2022-12-17 21:24:40 +0200

    Add post request createTask from flutter (#11)

--- peer_reminder_flutter/lib/tasks/service/ITaskService.dart
+++ peer_reminder_flutter/lib/tasks/service/ITaskService.dart
@@ -1,6 +1,7 @@
 import '../model/Task.dart';
+import 'package:http/http.dart' as http;
 
-abstract class TaskDBOps {
+abstract class ITaskService {
   Task getTaskByName(String taskName);
   Task getTaskById(int taskId);
   List<Task> getTaskList(int noOfTask);
@@ -8,4 +9,6 @@ abstract class TaskDBOps {
 
   // FIXME: return something else for deleteTask
   void deleteTask(Task task);
+
+  Future<http.Response> createTask(String taskJsonBody);
 }
