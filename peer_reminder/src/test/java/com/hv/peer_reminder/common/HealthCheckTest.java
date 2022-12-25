174603ebfc1efc215180b911f72d099ccda7e3e3
Tuan-Hung VU <41147045+TuanHungVU1202@users.noreply.github.com>
2022-12-25 14:20:29 +0200

    Build and Test for java Backend (#35) (#36)
    * Add test and report for ci
    
    * Add comments for test and report. Fix path to pom
    
    * Use default working directory
    
    * Add simple test
    
    * Done build and test for be java

---
+++ peer_reminder/src/test/java/com/hv/peer_reminder/common/HealthCheckTest.java
@@ -0,0 +1,13 @@
+package com.hv.peer_reminder.common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HealthCheckTest {
+    @Test
+    void sum() {
+        int sum = 5;
+        assertEquals(5, sum);
+    }
+}
