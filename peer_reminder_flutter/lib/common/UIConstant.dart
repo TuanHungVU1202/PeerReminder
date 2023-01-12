11630ebc744939f4f94af976188654ca6e3ad8a0
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2023-01-12 23:05:33 +0200

    Init SettingPage

--- peer_reminder_flutter/lib/common/UIConstant.dart
+++ peer_reminder_flutter/lib/common/UIConstant.dart
@@ -9,4 +9,11 @@ abstract class UIConstant {
   static Color kBackgroundColor = const Color(0xFFD2FFF4);
   static Color kPrimaryColor = const Color(0xFF2D5D70);
   static Color kSecondaryColor = const Color(0xFF265DAB);
+
+  // For SettingPage
+  static Color kMediumGrayColor = const Color(0xFFC7C7CC);
+  static Color kItemPressedColor = const Color(0xFFD9D9D9);
+  static Color kBorderColor = const Color(0xFFBCBBC1);
+  static Color kBackgroundGray = const Color(0xFFEFEFF4);
+  static Color kGroupSubtitle = const Color(0xFF777777);
 }
---
+++ peer_reminder_flutter/lib/settings/SettingsPage.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:peer_reminder_flutter/common/UIConstant.dart';
+
+import 'component/SettingsGroup.dart';
+import 'component/SettingsItem.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Container(
+        color: UIConstant.kBackgroundGray,
+        child: CustomScrollView(
+          slivers: <Widget>[
+            const CupertinoSliverNavigationBar(
+              largeTitle: Text('Settings'),
+            ),
+            SliverSafeArea(
+              top: false,
+              // This is just a big list of all the items in the settings.
+              sliver: SliverList(
+                delegate: SliverChildListDelegate(
+                  <Widget>[
+                    SettingsGroup(<Widget>[
+                      SettingsHeader(),
+                    ]),
+                    SettingsGroup(<Widget>[
+                      SettingsItem(
+                        label: 'Airplane Mode',
+                        iconAssetLabel: 'airplane',
+                        type: SettingsItemType.toggle,
+                      ),
+                      SettingsItem(
+                        label: 'Wi-Fi',
+                        iconAssetLabel: 'wifi',
+                        type: SettingsItemType.modal,
+                        value: 'Airport Free',
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Bluetooth',
+                        iconAssetLabel: 'bluetooth',
+                        type: SettingsItemType.modal,
+                        value: 'On',
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Cellular',
+                        iconAssetLabel: 'cellular',
+                        type: SettingsItemType.modal,
+                        onPress: () => Navigator.push(context, CellularPage.route()),
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Personal Hotspot',
+                        iconAssetLabel: 'hotspot',
+                        type: SettingsItemType.modal,
+                        value: 'Off',
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'VPN',
+                        iconAssetLabel: 'vpn',
+                        type: SettingsItemType.modal,
+                        value: 'Not Connected',
+                        hasDetails: true,
+                      ),
+                    ]),
+                    SettingsGroup(<Widget>[
+                      SettingsItem(
+                        label: 'Notifications',
+                        iconAssetLabel: 'notifications',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Control Center',
+                        iconAssetLabel: 'control_center',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Do Not Disturb',
+                        iconAssetLabel: 'disturb',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                    ]),
+                    SettingsGroup(<Widget>[
+                      SettingsItem(
+                        label: 'General',
+                        iconAssetLabel: 'general',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Display & Brightness',
+                        iconAssetLabel: 'display',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Wallpaper',
+                        iconAssetLabel: 'wallpaper',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Sounds & Haptics',
+                        iconAssetLabel: 'sounds',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Siri & Search',
+                        iconAssetLabel: 'siri',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Face ID & Passcode',
+                        iconAssetLabel: 'face_id',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Emergency SOS',
+                        iconAssetLabel: 'sos',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Battery',
+                        iconAssetLabel: 'battery',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Privacy',
+                        iconAssetLabel: 'privacy',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                    ]),
+                    SettingsGroup(<Widget>[
+                      SettingsItem(
+                        label: 'iTunes & App Store',
+                        iconAssetLabel: 'itunes',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Wallet & Apple Pay',
+                        iconAssetLabel: 'wallet',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                    ]),
+                    SettingsGroup(<Widget>[
+                      SettingsItem(
+                        label: 'Accounts & Passwords',
+                        iconAssetLabel: 'accounts',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Contacts',
+                        iconAssetLabel: 'contacts',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Calendar',
+                        iconAssetLabel: 'calendar',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
